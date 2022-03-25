71b5592ddd10c2026893491b2214561c77a67017
naeem <kazinaeemrayhan@gmail.com>
2022-03-25 21:59:02 +0600

    fakeData added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <h1>Laptop Finder</h1>
+      <h3>Choose 4 Laptop</h3>
+    </nav>
+  );
+};
+
+export default Header;
